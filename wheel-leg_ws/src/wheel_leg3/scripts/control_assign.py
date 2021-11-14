3bb465a1f81790ebf0bdab3a5592d38dba7732e0
jqYu <kar98kyjq@163.com>
2021-11-14 21:20:56 +0800

    直立环参数整定

--- wheel-leg_ws/src/wheel_leg3/scripts/control_assign.py
+++ wheel-leg_ws/src/wheel_leg3/scripts/control_assign.py
@@ -3,23 +3,29 @@ import rospy
 from std_msgs.msg import Float64
 
 
-total_velocity = 0.0
-total_roation = 0.0
+velocity1 = 0.0
+velocity2 = 0.0
 
-def total_velocity_cb(msg):
-    global total_velocity 
-    total_velocity = msg.data
+def velocity1_cb(msg):
+    global velocity1 
+    velocity1 = msg.data
 
-rospy.Subscriber('/balance_v_command',Float64,total_velocity_cb,queue_size=1)
+def velocity2_cb(msg):
+    global velocity2 
+    velocity2 = msg.data
+
+rospy.Subscriber('/balance_command1',Float64,velocity1_cb,queue_size=1)
+rospy.Subscriber('/balance_command2',Float64,velocity2_cb,queue_size=1)
 right_wheel_command_pub = rospy.Publisher('/wheel_leg3/wheel1_velocity_controller/command',Float64,queue_size=10)
 left_wheel_command_pub = rospy.Publisher('/wheel_leg3/wheel2_velocity_controller/command',Float64,queue_size=10)
 
 rospy.init_node('control_assign')
-r = rospy.Rate(100)
+r = rospy.Rate(200)
 
 while not rospy.is_shutdown():
-    right_cmd = Float64(total_velocity)
-    left_cmd = Float64(total_velocity)
+    velocity = velocity1 + velocity2
+    right_cmd = Float64(velocity)
+    left_cmd = Float64(velocity)
     right_wheel_command_pub.publish(right_cmd)
     left_wheel_command_pub.publish(left_cmd)
     r.sleep()
