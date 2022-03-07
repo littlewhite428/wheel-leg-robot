30776632a740f88e596d5c86255469a7992dd8d6
jqYu <kar98kyjq@163.com>
2022-03-07 17:01:18 +0800

    航向角控制模型改正

--- wheel-leg_ws/src/wheel_leg/simulink/control_parameter.m
+++ wheel-leg_ws/src/wheel_leg/simulink/control_parameter.m
@@ -3,6 +3,7 @@ l = 0.13; % 质心到转轴距离
 M_w  = 0.09293; % 单个轮子重
 I_w = 7.0742E-05; % 轮子绕转轴转动惯量
 I_p = 0.0035 + m * l^2; % 车身到转轴转动惯量 平行轴定理 
+I_pusai = 0.0132; %转向转动惯量
 r = 0.04; % 车轮半径
 g = 9.8; % 重力加速度
 D = 0.22; % 轮距
@@ -11,7 +12,8 @@ m11 = (m+2*M_w)*r + 2*(I_w)/r;
 m12 = m*l*r;
 m21 = m*l;
 m22 = I_p + m*l^2;
-m31 = (M_w+I_w*r^-2)*D;
+m31 = r*((M_w+I_w*r^-2)*D + 2*I_pusai/D);
+% m31 = (M_w+I_w*r^-2)*D;
 G = m*g*l;
 delta = m11*m22 - m21*m12;
 
@@ -28,7 +30,7 @@ rank(ctrb(A,B))
 % LQR
 % Q=diag([50,150,30,10]);R=diag([1,1]);
 % [K,S_tmp,E_tmp] = lqr(sys1.A,sys1.B,Q,R)
-
+% 
 % sys_d = c2d(sys1,Ts)
 % dlqr(sys_d.A,sys_d.B,Q,R)
 
@@ -42,7 +44,7 @@ sys_c_d = c2d(sys_c,Ts);
 
 A_s = [A  zeros(4,2);-b_c*C A_c];
 B_s = [B;-b_c*D];
-Q_s = diag([50,10,20,10,50,20]);R_s=diag([1,1]);
+Q_s = diag([50,10,20,10,50,50]);R_s=diag([1,1]);
 [KK,S_tmp,E_tmp] = lqr(A_s,B_s,Q_s,R_s)
 
 K=KK(:,1:4)
